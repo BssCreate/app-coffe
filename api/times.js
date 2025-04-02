56b381fc8a8b868b018feb0367163e14e70a3061
BssCreate <bed_random@mail.ru>
2025-04-02 22:32:11 +0700

    Update times.js

--- api/times.js
+++ api/times.js
@@ -1,3 +1,14 @@
+import fs from "fs";
+import path from "path";
+
 export default function handler(req, res) {
-    res.status(200).json(require("/times.json"));
+    try {
+        const filePath = path.join(process.cwd(), "times.json");
+        const jsonData = fs.readFileSync(filePath, "utf8");
+        const schedule = JSON.parse(jsonData);
+        res.status(200).json(schedule);
+    } catch (error) {
+        console.error("Ошибка API:", error);
+        res.status(500).json({ error: "Ошибка сервера" });
+    }
 }
